aa76b47bb43bda294d8632785861b74c96ffc09f
sami-theion <sami@theion.de>
2022-06-30 10:51:30 +0200

    added first pkg heating

---
+++ mainApp.cpp
@@ -0,0 +1,9 @@
+#include "wfg_v2/wfg_system/sulfur_heating_system/heating_system.h"
+int main() {
+    // Write C++ code here
+    client user;
+    user.run_heating_system();
+    user.close_heating_system();
+
+    return 0;
+}
