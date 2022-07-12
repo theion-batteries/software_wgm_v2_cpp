a95a0c6255e8c308893bb183081ce9a06a480402
sami-theion <sami@theion.de>
2022-07-12 16:44:00 +0200

    updated folder tree

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # software_wgm_v2_cpp
-contains the wafer growing machine v2 [source code](/wfg_v2/) and [documentation](/wfg_system_modelling.pdf)
+contains the wafer growing machine v2 [source code](/wfg_v2/) and [documentation](wfg_system_modelling/wfg_system_modelling.pdf)
--- wfg_v2/process_manager.h
+++ wfg_v2/process_manager.h
@@ -75,47 +75,14 @@ namespace wgm_processes
         wgm_feedbacks::proc_feedback process_feedback;
         std::string process_name = "heating process";
     public:
-        heating_process() {
-            heating_sys = new sulfur_heating_system::sulfur_heating_controller();
-            process_timer = new wgm_monitoring::time_monitor();
-            process_temp_monitor = new wgm_monitoring::heat_monitor(heating_sys);
-        }
-
-        virtual ~heating_process()
-        {
-            std::cout << "deleting heating process " << std::endl;
-            delete heating_sys;
-            delete process_timer;
-            delete process_temp_monitor;
-        }
+        heating_process(); 
+        virtual ~heating_process();
         virtual void start_process();
         virtual void stop_process();
         virtual std::string get_name() { return process_name; };
         virtual bool is_proc_success() { return process_feedback.report_feedback(); };
     };
-    void heating_process::start_process()
-    {
-        std::cout << "execute " << process_name << std::endl;
-        // start timer
-        process_timer->start_monitoring();
-        // start system
-        heating_sys->turn_on_heating();
-        // report feedback
-        // feedback
-        // stop timer
-        process_timer->stop_monitoring();
-        // start temp monitor
-        process_temp_monitor->start_monitoring();
 
-
-    }
-    void heating_process::stop_process()
-    {
-        std::cout << "finish " << process_name << std::endl;
-        heating_sys->turn_off_heating();
-        process_temp_monitor->stop_monitoring();
-
-    }
     /******************* interface wafer insertion process***************/
     class Isinking_process : public Iprocesses_managment
     {
@@ -143,45 +110,20 @@ namespace wgm_processes
         std::string process_name = "sinking process";
 
     public:
-        sinking_process() {
-            sinking_sys = new wafer_holder_motion_system::wafer_motion_controller();
-            process_timer = new wgm_monitoring::time_monitor();
-            process_dist_monitor = new wgm_monitoring::distance_monitor(sinking_sys);
-        }
-        virtual ~sinking_process()
-        {
-            std::cout << "deleting sinking process " << std::endl;
-            delete sinking_sys;
-            delete process_timer;
-            delete process_dist_monitor;
-        }
+        sinking_process();
+        virtual ~sinking_process();
         virtual void start_process();
         virtual void stop_process();
         virtual std::string get_name() { return process_name; };
         virtual bool is_proc_success() { return process_feedback.report_feedback(); };
     };
-    void sinking_process::start_process()
-    {
-        std::cout << "execute " << process_name << std::endl;
-        process_timer->start_monitoring();
-        sinking_sys->set_distance_to_surface_contact(30);
-        sinking_sys->insert_wafer_in_ml();
-        // feedback
-        process_timer->stop_monitoring();
-        process_dist_monitor->start_monitoring();
-    }
-    void sinking_process::stop_process()
-    {
-        std::cout << "finish " << process_name << std::endl;
-        process_dist_monitor->stop_monitoring();
-    }
+
     /****************** interface cnt alignment process*******************/
     class Ialigning_process : public Iprocesses_managment
     {
     public:
         Ialigning_process()
         {
-            std::cout << "creating aligning process " << std::endl;
         }
         virtual ~Ialigning_process()
         {
@@ -203,44 +145,14 @@ namespace wgm_processes
         std::string process_name = "aligning process";
 
     public:
-        aligning_process() {
-            aligning_sys = new cnt_alignment_system::cnt_aligning_controller();
-            process_timer = new wgm_monitoring::time_monitor();
-            process_curr_monitor = new wgm_monitoring::current_monitor(aligning_sys);
-            process_volt_monitor = new wgm_monitoring::voltage_monitor(aligning_sys);
-        }
-        virtual ~aligning_process()
-        {
-            std::cout << "deleting aligning process " << std::endl;
-            delete aligning_sys;
-            delete process_timer;
-            delete process_curr_monitor;
-            delete process_volt_monitor;
-        }
+        aligning_process();
+        virtual ~aligning_process();
         virtual void start_process();
         virtual void stop_process();
         virtual std::string get_name() { return process_name; };
         virtual bool is_proc_success() { return process_feedback.report_feedback(); };
 
     };
-
-    void aligning_process::start_process()
-    {
-        std::cout << "execute " << process_name << std::endl;
-        process_timer->start_monitoring();
-        aligning_sys->start_aligning();
-        // feedback
-        process_timer->stop_monitoring();
-        process_curr_monitor->start_monitoring();
-        process_volt_monitor->start_monitoring();
-    }
-    void aligning_process::stop_process()
-    {
-        std::cout << "finish " << process_name << std::endl;
-        aligning_sys->stop_aligning();
-        process_curr_monitor->stop_monitoring();
-        process_volt_monitor->stop_monitoring();
-    }
     /****************** interface cooling process *******************/
     class Icooling_process : public Iprocesses_managment
     {
