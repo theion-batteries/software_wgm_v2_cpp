396b57839585c54376946fea2720e54cdc77c599
sami-theion <sami@theion.de>
2022-07-12 17:34:39 +0200

    switched to make build

---
+++ build/CMakeFiles/wgm_v2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wgm_v2.dir/C_/Users/SamiDhiab/Theion_Repos/software_wgm_v2_cpp/src/mainApp.cpp.obj"
+  "CMakeFiles/wgm_v2.dir/C_/Users/SamiDhiab/Theion_Repos/software_wgm_v2_cpp/src/mainApp.cpp.obj.d"
+  "libwgm_v2.dll.a"
+  "wgm_v2.exe"
+  "wgm_v2.exe.manifest"
+  "wgm_v2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wgm_v2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
