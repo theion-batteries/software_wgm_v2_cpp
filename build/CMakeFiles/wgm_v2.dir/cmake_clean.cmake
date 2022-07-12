39c000c0e8bbd7ffc755b675f26cf2a37a32bda4
sami-theion <sami@theion.de>
2022-07-12 23:35:18 +0200

    added bin folder

--- build/CMakeFiles/wgm_v2.dir/cmake_clean.cmake
+++ build/CMakeFiles/wgm_v2.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
 file(REMOVE_RECURSE
+  "C:/Users/SamiDhiab/Theion_Repos/software_wgm_v2_cpp/bin/wgm_v2.exe"
+  "C:/Users/SamiDhiab/Theion_Repos/software_wgm_v2_cpp/bin/wgm_v2.exe.manifest"
+  "C:/Users/SamiDhiab/Theion_Repos/software_wgm_v2_cpp/bin/wgm_v2.pdb"
   "CMakeFiles/wgm_v2.dir/C_/Users/SamiDhiab/Theion_Repos/software_wgm_v2_cpp/src/main.cpp.obj"
   "CMakeFiles/wgm_v2.dir/C_/Users/SamiDhiab/Theion_Repos/software_wgm_v2_cpp/src/main.cpp.obj.d"
   "libwgm_v2.dll.a"
-  "wgm_v2.exe"
-  "wgm_v2.exe.manifest"
-  "wgm_v2.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
