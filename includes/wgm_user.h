70a9088898da1970aee6c95ae1e7b9b2c136bff8
sami-theion <sami@theion.de>
2022-11-18 18:10:07 +0100

    added user class

--- includes/wgm_user.h
+++ includes/wgm_user.h
@@ -16,15 +16,23 @@
 /****************** user code ********************/
 namespace wgm_user
 {
-    
+
     class user {
     private:
         wgm_processes::Iprocesses_managment* processInterfaces;
+        std::string user_name;
+        std::string user_passowrd;
 
     public:
         user();
         void run_app();
         void stop_app();
+        std::string getUserName();
+        std::string getUserPassowrd();
+        void setUserName(std::string name);
+        void setUserPassowrd(std::string password);
+        void login();
+        void logout();
         ~user();
     };
 
--- src/wgm_user.cpp
+++ src/wgm_user.cpp
@@ -30,3 +30,29 @@ wgm_user::user::~user()
   std::cout << "user stopped app" << std::endl;
   delete processInterfaces;
 }
+std::string wgm_user::user::getUserName()
+{
+  return user_name;
+}
+std::string wgm_user::user::getUserPassowrd()
+{
+  return user_passowrd;
+
+}
+void wgm_user::user::setUserName(std::string name)
+{
+  user_name = name;
+}
+void wgm_user::user::setUserPassowrd(std::string password)
+{
+  user_passowrd = password;
+}
+
+void wgm_user::user::login() 
+{
+  // enter user_name and passsword
+}
+void wgm_user::user::logout()
+{
+
+}
